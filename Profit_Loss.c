58704e03bc03173285713420d01d7de9241624b9
tanujn45 <tanujn45@gmail.com>
2016-09-01 20:25:06 +0530

    Update and rename prolos.c to Profit_Loss.c

--- Profit_Loss.c
+++ Profit_Loss.c
@@ -3,6 +3,8 @@ Author__________Tanuj Namdeo
 Program_________Profit & Loss
 email___________tanujn45@gmail.com
 
+Note: copy the code in a file and name it prolos.c
+
 Note: To compile the file in Ubuntu use the command below:
 gcc -o prolos prolos.c
 
